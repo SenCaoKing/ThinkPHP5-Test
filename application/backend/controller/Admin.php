31ed55ec985c621412eab8cbc787f519b0b4b6fc
SenCaoKing <1134349927@qq.com>
2018-10-26 14:35:51 +0800

    管理员删除及修改

--- application/backend/controller/Admin.php
+++ application/backend/controller/Admin.php
@@ -1,15 +1,14 @@
 <?php
 namespace app\backend\controller;
-use think\Controller;
-use think\Db;
-use think\Loader;
 
-class Admin extends Controller
-{
+use think\Loader;
+use think\Db;
+
+class Admin extends Base {
     // 管理员列表
     public function index(){
         $list = array();
-        $keywods = I('keywords/s');
+        $keywods = input('keywords/s');
         if(empty($keywods)){ // 开发者测试账号，测试用
             $res = Db::name('admin')->where('admin_id', 'not in', '2, 3')->select();
         }else{
@@ -27,14 +26,20 @@ class Admin extends Controller
     }
 
     public function admin_info(){
-
+        $admin_id = input('admin_id/d', 0);
+        if($admin_id){
+            $info = Db::name('admin')->where('admin_id', $admin_id)->find();
+            $info['password'] = "";
+            $this->assign('info', $info);
+        }
+        $act = empty($admin_id) ? 'add' : 'edit';
+        $this->assign('act', $act);
 
         return $this->fetch();
     }
 
     public function adminHandle(){
-        $data =I('post.');
-
+        $data = I('post.');
         $adminValidate = Loader::validate('Admin');
         if(!$adminValidate->scene($data['act'])->batch()->check($data)){
             $this->ajaxReturn(['status'=>-1, 'msg'=>'操作失败', 'result'=>$adminValidate->getError()]);
@@ -45,24 +50,22 @@ class Admin extends Controller
             $data['password'] = encrypt($data['password']);
         }
 
-        // $this->ajaxReturn($data);
 
         if($data['act'] == 'add'){
             unset($data['admin_id'], $data['act'], $data['auth_code']);
             $data['add_time'] = time();
             $res = Db::name('admin')->insert($data);
         }
+
         if($data['act'] == 'del' && $data['admin_id'] > 1){
             $res = Db::name('admin')->where('admin_id', $data['admin_id'])->delete();
         }
 
         if($res){
-            $this->ajaxReturn(['status'=>1, 'msg'=>'操作成功', 'url'=>url('Backend/Admin/index')]);
+            $this->ajaxReturn(['status'=>1, 'msg'=>'操作成功', 'url'=>url('Admin/index')]);
         }else{
             $this->ajaxReturn(['status'=>-1, 'msg'=>'操作失败']);
         }
-
-
     }
 
     public function ajaxReturn($data, $type='json'){
---
+++ application/backend/controller/Base.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\backend\controller;
+use think\Controller;
+use think\Db;
+
+class Base extends Controller
+{
+    /**
+     * 析构函数
+     */
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+}
